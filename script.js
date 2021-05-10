2d97507095812f2eb6abaa43028d48e3dd660536
Vincent <vshan0816@gmail.com>
2021-05-10 15:28:20 -0400

    Finished website

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-document.querySelector('body').appendChild(h2); 
+function rotateImg() {
+    document.querySelector("#infernoDivider").style.transform = "rotate(90deg)";
+  }
